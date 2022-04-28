62de2f1176f2d959c6250476dda696906740a0a2
ThunderVVV <zhangjinglei168@sjtu.edu.cn>
2022-04-28 14:29:55 +0000

    add window and lmdb config

---
+++ configs/selfsup/_base_/datasets/lmdb_synth_simclr.py
@@ -0,0 +1,36 @@
+# dataset settings
+data_source = 'Lmdb'
+dataset_type = 'MultiViewDataset'
+img_norm_cfg = dict(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+train_pipeline = [
+    dict(type='Resize', size=[32, 100]),
+    dict(type='MyTransform'),
+    ]
+
+# prefetch
+prefetch = False
+if not prefetch:
+    train_pipeline.extend(
+        [dict(type='ToTensor'),
+         dict(type='Normalize', **img_norm_cfg)])
+
+# dataset summary
+data = dict(
+    samples_per_gpu=256,  # total 256*4
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        # data_source=dict(
+        #     type=data_source,
+        #     data_prefix='data/synth',
+        #     ann_file='data/synth.txt',
+        # ),
+        data_source=dict(
+            type=data_source,
+            data_prefix='data/lmdb/synth',
+            ann_file='data/lmdb/synth',
+        ),
+        num_views=[2],
+        pipelines=[train_pipeline],
+        prefetch=prefetch,
+    ))
